feda075fe5cae1f23f7ead119e004f097f0f31e5
rmtrigo <rmontrigo@gmail.com>
2015-09-02 02:20:41 -0700

    2: JavaScript Basics - Earned Variables badge

---
+++ javascript_basics/stage2challenge/story.js
@@ -0,0 +1,9 @@
+var noun = prompt("Enter a noun:");
+var verb = prompt("Enter a verb:");
+var adjective = prompt("Enter an adjective:");
+
+var message = "<h2>The " + adjective + " " + noun + " went to go " +
+			  verb + ".</h2>";
+document.write(message);
+
+
---
+++ javascript_basics/stage2vid1/scripts.js
@@ -0,0 +1,9 @@
+var message = "Hello!";
+alert(message);
+message = "Welcome to JavaScript Basics";
+alert(message);
+message = "She's a great person!";
+var htmlSnippet = '<h1 class="special">Important Headline</h1>';
+
+message = 'She\s a great person!'
+htmlSnippet = "<h1 class\"special\">Important Headline</h1>";
