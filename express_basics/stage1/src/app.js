2788d266f1ce54d5ffa9a12c5bb3fe214da262a0
rmtrigo <rmontrigo@gmail.com>
2015-09-30 21:03:04 -0700

    22: Express Basics - Earned Getting Started with Express badge

---
+++ express_basics/stage1/src/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send("<h1>I Love Treehouse!</h1>");
+});
+
+app.listen(3000, function() {
+	console.log("The frontend server is runnning on port 3000!");
+});
